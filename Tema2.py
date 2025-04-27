0e8f8cf8fb9d1613c605144fd1674b804a8bd997
Paul <cureu.paul.ioan@gmail.com>
2025-04-27 13:38:35 +0300

    update repository

--- Tema2.py
+++
@@ -1,5 +0,0 @@
-phrase = '''    Borsec este o marcă de apă minerală îmbuteliată creată
-de către colonelul austriac Valentin Gunther și vărul său geolog
-Anton Zimmethausen în 1806.[1] În momentul de față este unul dintre
-cele mai puternice branduri din România.'''
-print(phrase[:len(phrase) // 2].translate(str.maketrans(" ", " ")).upper().strm     ip() + phrase[len(phrase) // 2::-1].capitalize().translate(str.maketrans("", "", ".,!?")))
